63c8ef0e1c46f687df6eb1ef04c0e6d830376eb1
Samuel Nti Sarpong <samuel.n.sarpong@gmail.com>
2025-07-23 07:48:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- user/project/src/lib/firebase/client.ts
+++ user/project/src/lib/firebase/client.ts
@@ -1,3 +1,4 @@
+
 // src/lib/firebase/client.ts
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
@@ -11,7 +12,6 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
