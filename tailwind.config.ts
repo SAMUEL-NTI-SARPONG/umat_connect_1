a4b0f8cc1b2c65b7d1b302fb66b4dfed4ee5a76e
Samuel Nti Sarpong <samuel.n.sarpong@gmail.com>
2025-08-02 07:03:43 +0000

    That is not the color. Please check it well.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,6 +69,9 @@ export default {
         'exam-day': '#03A6A1',
         'selected-day': '#ED3500',
       },
+      borderColor: {
+        'selected-day': '#ED3500',
+      },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
