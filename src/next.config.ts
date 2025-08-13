2e205efc89c779a02f7c84418c627dd4c0f286e8
Samuel Nti Sarpong <samuel.n.sarpong@gmail.com>
2025-08-13 20:13:53 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.ts
@@ -0,0 +1,34 @@
+
+import type {NextConfig} from 'next';
+// import withPWAInit from "@ducanh2912/next-pwa";
+
+// const withPWA = withPWAInit({
+//   dest: "public",
+//   disable: process.env.NODE_ENV === "development",
+//   register: true,
+//   skipWaiting: true,
+//   sw: 'service-worker.js',
+// });
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
+// export default withPWA(nextConfig);
